1b0dbf9e3725af54343fb8a53a104aaaf648ac17
naveen <naveen@rivervalleytechnologies.in>
2025-05-27 15:48:34 +0530

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'strong-password';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
 })
